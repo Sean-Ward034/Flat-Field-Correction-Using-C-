c9cfa7c5d926d181319a0354b5ae7dacef6f57f4
Sean-Ward034 <sward47@vols.utk.edu>
2024-09-30 19:50:48 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace FlatFieldCorrectionApp
+{
+    partial class Form1
+    {
+        private System.Windows.Forms.PictureBox pictureBoxObject;
+        private System.Windows.Forms.PictureBox pictureBoxCorrected;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnApplyCorrection;
+        private System.Windows.Forms.Button btnEditCorrection;
+        private System.Windows.Forms.Button btnSaveChanges;
+        private System.Windows.Forms.Button btnDiscardChanges;
+        private System.Windows.Forms.TrackBar trackBarBrightness;
+        private System.Windows.Forms.TrackBar trackBarContrast;
+        private System.Windows.Forms.TrackBar trackBarExposure;
+        private System.Windows.Forms.Label lblBrightness;
+        private System.Windows.Forms.Label lblContrast;
+        private System.Windows.Forms.Label lblExposure;
+
+        private void InitializeComponent()
+        {
+            this.pictureBoxObject = new System.Windows.Forms.PictureBox();
+            this.pictureBoxCorrected = new System.Windows.Forms.PictureBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnApplyCorrection = new System.Windows.Forms.Button();
+            this.btnEditCorrection = new System.Windows.Forms.Button();
+            this.btnSaveChanges = new System.Windows.Forms.Button();
+            this.btnDiscardChanges = new System.Windows.Forms.Button();
+            this.trackBarBrightness = new System.Windows.Forms.TrackBar();
+            this.trackBarContrast = new System.Windows.Forms.TrackBar();
+            this.trackBarExposure = new System.Windows.Forms.TrackBar();
+            this.lblBrightness = new System.Windows.Forms.Label();
+            this.lblContrast = new System.Windows.Forms.Label();
+            this.lblExposure = new System.Windows.Forms.Label();
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxObject)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCorrected)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBrightness)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarContrast)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarExposure)).BeginInit();
+
+            this.SuspendLayout();
+
+            // PictureBox for the Object Image
+            this.pictureBoxObject.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxObject.Location = new System.Drawing.Point(30, 30);
+            this.pictureBoxObject.Size = new System.Drawing.Size(400, 400);
+            this.pictureBoxObject.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+
+            // PictureBox for the Corrected Image
+            this.pictureBoxCorrected.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxCorrected.Location = new System.Drawing.Point(500, 30);
+            this.pictureBoxCorrected.Size = new System.Drawing.Size(400, 400);
+            this.pictureBoxCorrected.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+
+            // Button for Upload
+            this.btnUpload.Location = new System.Drawing.Point(30, 460);
+            this.btnUpload.Size = new System.Drawing.Size(150, 40);
+            this.btnUpload.Text = "Upload Image";
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+
+            // Button for Apply Correction
+            this.btnApplyCorrection.Location = new System.Drawing.Point(210, 460);
+            this.btnApplyCorrection.Size = new System.Drawing.Size(150, 40);
+            this.btnApplyCorrection.Text = "Apply Correction";
+            this.btnApplyCorrection.Click += new System.EventHandler(this.btnApplyCorrection_Click);
+
+            // Button for Edit Correction
+            this.btnEditCorrection.Location = new System.Drawing.Point(390, 460);
+            this.btnEditCorrection.Size = new System.Drawing.Size(150, 40);
+            this.btnEditCorrection.Text = "Edit Correction";
+            this.btnEditCorrection.Enabled = false; // Initially disabled
+            this.btnEditCorrection.Click += new System.EventHandler(this.btnEditCorrection_Click);
+
+            // Button for Save Changes
+            this.btnSaveChanges.Location = new System.Drawing.Point(570, 460);
+            this.btnSaveChanges.Size = new System.Drawing.Size(150, 40);
+            this.btnSaveChanges.Text = "Save Changes";
+            this.btnSaveChanges.Enabled = false; // Initially disabled
+            this.btnSaveChanges.Click += new System.EventHandler(this.btnSaveChanges_Click);
+
+            // Button for Discard Changes
+            this.btnDiscardChanges.Location = new System.Drawing.Point(750, 460);
+            this.btnDiscardChanges.Size = new System.Drawing.Size(150, 40);
+            this.btnDiscardChanges.Text = "Discard Changes";
+            this.btnDiscardChanges.Enabled = false; // Initially disabled
+            this.btnDiscardChanges.Click += new System.EventHandler(this.btnDiscardChanges_Click);
+
+            // TrackBar for Brightness Adjustment
+            this.trackBarBrightness.Location = new System.Drawing.Point(30, 520);
+            this.trackBarBrightness.Minimum = -100;
+            this.trackBarBrightness.Maximum = 100;
+            this.trackBarBrightness.TickFrequency = 10;
+            this.trackBarBrightness.Enabled = false;
+            this.trackBarBrightness.ValueChanged += new System.EventHandler(this.trackBarBrightness_ValueChanged);
+
+            // Label for Brightness
+            this.lblBrightness.Location = new System.Drawing.Point(30, 500);
+            this.lblBrightness.Text = "Brightness";
+
+            // TrackBar for Contrast Adjustment
+            this.trackBarContrast.Location = new System.Drawing.Point(210, 520);
+            this.trackBarContrast.Minimum = -100;
+            this.trackBarContrast.Maximum = 100;
+            this.trackBarContrast.TickFrequency = 10;
+            this.trackBarContrast.Enabled = false;
+            this.trackBarContrast.ValueChanged += new System.EventHandler(this.trackBarContrast_ValueChanged);
+
+            // Label for Contrast
+            this.lblContrast.Location = new System.Drawing.Point(210, 500);
+            this.lblContrast.Text = "Contrast";
+
+            // TrackBar for Exposure Adjustment
+            this.trackBarExposure.Location = new System.Drawing.Point(390, 520);
+            this.trackBarExposure.Minimum = -100;
+            this.trackBarExposure.Maximum = 100;
+            this.trackBarExposure.TickFrequency = 10;
+            this.trackBarExposure.Enabled = false;
+            this.trackBarExposure.ValueChanged += new System.EventHandler(this.trackBarExposure_ValueChanged);
+
+            // Label for Exposure
+            this.lblExposure.Location = new System.Drawing.Point(390, 500);
+            this.lblExposure.Text = "Exposure";
+
+            // Form Settings
+            this.ClientSize = new System.Drawing.Size(950, 600);
+            this.Controls.Add(this.pictureBoxObject);
+            this.Controls.Add(this.pictureBoxCorrected);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnApplyCorrection);
+            this.Controls.Add(this.btnEditCorrection);
+            this.Controls.Add(this.btnSaveChanges);
+            this.Controls.Add(this.btnDiscardChanges);
+            this.Controls.Add(this.trackBarBrightness);
+            this.Controls.Add(this.trackBarContrast);
+            this.Controls.Add(this.trackBarExposure);
+            this.Controls.Add(this.lblBrightness);
+            this.Controls.Add(this.lblContrast);
+            this.Controls.Add(this.lblExposure);
+            this.Name = "Form1";
+            this.Text = "Advanced Flat Field Correction App";
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxObject)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCorrected)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBrightness)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarContrast)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarExposure)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FlatFieldCorrectionApp
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap objectImage;
+        private Bitmap darkFrameImage;
+        private Bitmap brightFrameImage;
+        private Bitmap correctedImage;  // The original corrected image
+        private Bitmap editedImage;     // The image that is being edited
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.bmp;*.jpg;*.jpeg;*.png";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    objectImage = new Bitmap(openFileDialog.FileName);
+                    pictureBoxObject.Image = objectImage;
+
+                    GenerateCalibrationImages(objectImage);
+
+                    MessageBox.Show("Dark and Bright Field Images generated and smoothed automatically.");
+                }
+            }
+        }
+
+        private void btnApplyCorrection_Click(object sender, EventArgs e)
+        {
+            if (objectImage == null)
+            {
+                MessageBox.Show("Please upload an object image first.");
+                return;
+            }
+
+            // Apply flat-field correction
+            correctedImage = ApplyFlatFieldCorrection(objectImage, darkFrameImage, brightFrameImage);
+            pictureBoxCorrected.Image = correctedImage;
+
+            // Enable the "Edit Correction" button
+            btnEditCorrection.Enabled = true;
+        }
+
+        private void btnEditCorrection_Click(object sender, EventArgs e)
+        {
+            if (correctedImage == null) return;
+
+            // Make a copy of the corrected image to start editing
+            editedImage = new Bitmap(correctedImage);
+
+            // Enable the trackbars for editing
+            trackBarBrightness.Enabled = true;
+            trackBarContrast.Enabled = true;
+            trackBarExposure.Enabled = true;
+
+            // Enable "Save Changes" and "Discard Changes" buttons
+            btnSaveChanges.Enabled = true;
+            btnDiscardChanges.Enabled = true;
+        }
+
+        private void trackBarBrightness_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateImageAdjustments();
+        }
+
+        private void trackBarContrast_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateImageAdjustments();
+        }
+
+        private void trackBarExposure_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateImageAdjustments();
+        }
+
+        private void btnSaveChanges_Click(object sender, EventArgs e)
+        {
+            if (editedImage != null)
+            {
+                // Save changes by setting correctedImage to editedImage
+                correctedImage = new Bitmap(editedImage);
+                pictureBoxCorrected.Image = correctedImage;
+
+                // Reset UI controls after saving
+                ResetEditingControls();
+            }
+        }
+
+        private void btnDiscardChanges_Click(object sender, EventArgs e)
+        {
+            if (correctedImage != null)
+            {
+                // Discard changes by restoring the original corrected image
+                pictureBoxCorrected.Image = correctedImage;
+
+                // Reset UI controls after discarding
+                ResetEditingControls();
+            }
+        }
+
+        private void ResetEditingControls()
+        {
+            // Disable the editing controls
+            trackBarBrightness.Enabled = false;
+            trackBarContrast.Enabled = false;
+            trackBarExposure.Enabled = false;
+
+            // Disable the save/discard buttons
+            btnSaveChanges.Enabled = false;
+            btnDiscardChanges.Enabled = false;
+
+            // Reset trackbars to default values
+            trackBarBrightness.Value = 0;
+            trackBarContrast.Value = 0;
+            trackBarExposure.Value = 0;
+        }
+
+        private void UpdateImageAdjustments()
+        {
+            if (editedImage == null) return;
+
+            // Get current slider values
+            float brightness = trackBarBrightness.Value / 100.0f;
+            float contrast = trackBarContrast.Value / 100.0f;
+            float exposure = trackBarExposure.Value / 100.0f;
+
+            // Apply adjustments to the edited image
+            Bitmap adjustedImage = AdjustImage(correctedImage, brightness, contrast, exposure);
+            editedImage = adjustedImage;
+
+            // Show the adjusted image in the picture box
+            pictureBoxCorrected.Image = adjustedImage;
+        }
+
+        private Bitmap AdjustImage(Bitmap image, float brightness, float contrast, float exposure)
+        {
+            Bitmap adjustedImage = new Bitmap(image.Width, image.Height);
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+
+                    // Apply brightness, contrast, and exposure adjustments
+                    int r = Clamp((int)(pixel.R * (1 + brightness) * (1 + contrast + exposure)), 0, 255);
+                    int g = Clamp((int)(pixel.G * (1 + brightness) * (1 + contrast + exposure)), 0, 255);
+                    int b = Clamp((int)(pixel.B * (1 + brightness) * (1 + contrast + exposure)), 0, 255);
+
+                    adjustedImage.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            return adjustedImage;
+        }
+
+        private void GenerateCalibrationImages(Bitmap objectImage)
+        {
+            darkFrameImage = CreateDarkField(objectImage);
+            brightFrameImage = CreateBrightField(objectImage);
+
+            // Apply Gaussian Smoothing
+            darkFrameImage = ApplyGaussianSmoothing(darkFrameImage);
+            brightFrameImage = ApplyGaussianSmoothing(brightFrameImage);
+        }
+
+        private Bitmap CreateDarkField(Bitmap objectImage)
+        {
+            return AdjustBrightness(objectImage, 0.2f); // Reduce brightness by 80%
+        }
+
+        private Bitmap CreateBrightField(Bitmap objectImage)
+        {
+            return AdjustBrightness(objectImage, 1.5f); // Increase brightness by 50%
+        }
+
+        private Bitmap AdjustBrightness(Bitmap image, float factor)
+        {
+            Bitmap adjustedImage = new Bitmap(image.Width, image.Height);
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+                    int adjustedR = Clamp((int)(pixel.R * factor), 0, 255);
+                    int adjustedG = Clamp((int)(pixel.G * factor), 0, 255);
+                    int adjustedB = Clamp((int)(pixel.B * factor), 0, 255);
+                    adjustedImage.SetPixel(x, y, Color.FromArgb(adjustedR, adjustedG, adjustedB));
+                }
+            }
+            return adjustedImage;
+        }
+
+        private Bitmap ApplyFlatFieldCorrection(Bitmap rawImage, Bitmap darkFrame, Bitmap brightFrame)
+        {
+            int width = rawImage.Width;
+            int height = rawImage.Height;
+            Bitmap correctedImage = new Bitmap(width, height);
+
+            float darkMean = CalculateMeanIntensity(darkFrame);
+            float brightMean = CalculateMeanIntensity(brightFrame);
+            float gain = Math.Max(brightMean - darkMean, 1); // Prevent division by zero
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color rawPixel = rawImage.GetPixel(x, y);
+                    Color darkPixel = darkFrame.GetPixel(x, y);
+                    Color brightPixel = brightFrame.GetPixel(x, y);
+
+                    int correctedR = (int)(((rawPixel.R - darkPixel.R) / gain) * 255);
+                    int correctedG = (int)(((rawPixel.G - darkPixel.G) / gain) * 255);
+                    int correctedB = (int)(((rawPixel.B - darkPixel.B) / gain) * 255);
+
+                    correctedImage.SetPixel(x, y, Color.FromArgb(
+                        Clamp(correctedR, 0, 255),
+                        Clamp(correctedG, 0, 255),
+                        Clamp(correctedB, 0, 255)));
+                }
+            }
+
+            return correctedImage;
+        }
+
+        private Bitmap ApplyGaussianSmoothing(Bitmap image)
+        {
+            Bitmap smoothed = new Bitmap(image.Width, image.Height);
+
+            int[,] kernel = {
+                { 1, 2, 1 },
+                { 2, 4, 2 },
+                { 1, 2, 1 }
+            };
+            int kernelSum = 16;
+
+            for (int y = 1; y < image.Height - 1; y++)
+            {
+                for (int x = 1; x < image.Width - 1; x++)
+                {
+                    int[] sums = new int[3]; // For R, G, B channels
+
+                    for (int ky = -1; ky <= 1; ky++)
+                    {
+                        for (int kx = -1; kx <= 1; kx++)
+                        {
+                            Color neighbor = image.GetPixel(x + kx, y + ky);
+                            int kernelValue = kernel[ky + 1, kx + 1];
+                            sums[0] += neighbor.R * kernelValue;
+                            sums[1] += neighbor.G * kernelValue;
+                            sums[2] += neighbor.B * kernelValue;
+                        }
+                    }
+
+                    smoothed.SetPixel(x, y, Color.FromArgb(
+                        Clamp(sums[0] / kernelSum, 0, 255),
+                        Clamp(sums[1] / kernelSum, 0, 255),
+                        Clamp(sums[2] / kernelSum, 0, 255)));
+                }
+            }
+
+            return smoothed;
+        }
+
+        private float CalculateMeanIntensity(Bitmap image)
+        {
+            float sum = 0;
+            int pixelCount = image.Width * image.Height;
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+                    sum += (pixel.R + pixel.G + pixel.B) / 3.0f;
+                }
+            }
+            return sum / pixelCount;
+        }
+
+        private int Clamp(int value, int min, int max)
+        {
+            return value < min ? min : value > max ? max : value;
+        }
+    }
+}
